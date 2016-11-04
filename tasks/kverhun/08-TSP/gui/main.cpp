b7f25c15ee932a995037cd91657117c8561acb20
kverhun <k.verhun@gmail.com>
2016-11-05 00:14:57 +0200

    Add drawing update while genetic algo is caclulated

--- tasks/kverhun/08-TSP/gui/main.cpp
+++ tasks/kverhun/08-TSP/gui/main.cpp
@@ -3,11 +3,28 @@
 #include <QApplication>
 #include <QtWidgets>
 #include <QPainter>
+#include <Qfuture>
+#include <QFutureWatcher>
+#include <QtConcurrent/qtconcurrentrun.h>
 
 #include <08-TSP\impl\TSP.h>
+#include <08-TSP\impl\PredefinedConfigurations.h>
+#include <08-TSP\impl\GeneticAlgo.h>
+
+#include <thread>
 
 namespace
 {
+    TSP::TPoints _TransposePoints(const TSP::TPoints& i_points)
+    {
+        auto result = i_points;
+        for (auto& pt : result)
+        {
+            std::swap(pt.x, pt.y);
+        }
+        return result;
+    }
+    
     using TPoint = std::pair<double, double>;
 
     TPoint operator + (const TPoint& i_pt1, const TPoint& i_pt2)
@@ -115,6 +132,9 @@ namespace
 
         void _DrawPath(QPainter& i_painter)
         {
+            if (m_current_path.size() != m_points_to_draw.size())
+                return;
+
             i_painter.setPen(QPen(Qt::red, 2));
             for (size_t i = 1; i < m_current_path.size(); ++i)
                 i_painter.drawLine(
@@ -127,6 +147,12 @@ namespace
                 );
         }
 
+        void resizeEvent(QResizeEvent* ip_resize_event)
+        {
+            QWidget::resizeEvent(ip_resize_event);
+            _CalculatePointsToDraw();
+        }
+
     private:
         const TSP& m_tsp;
         const size_t m_margin_x = 10;
@@ -136,28 +162,57 @@ namespace
         TSP::TPath m_current_path;
     };
 
+
+    TSP::TPath _PerformNextStepAndReturnBestPath(std::shared_ptr<TSPGenetic::GeneticSolver> ip_solver)
+    {
+        ip_solver->PerformSingleStep();
+        return ip_solver->GetCurrentPopulation().front();
+    }
+
 }
 
 int main(int i_argc, char** i_argv)
 {
     std::cout << "Start application..." << std::endl;
 
-    TSP::TPoints points = { { 1., 1. },{ 1., 2. },{ 2., 1. },{ 2., 2. },{ 3., 1. },{ 3., 2. } };
+    TSP::TPoints points = _TransposePoints(PredefiedConfigurations::g_config_states);
     TSP tsp(points);
 
     QApplication app(i_argc, i_argv);
     
     QPointer<DrawingWidget> p_widget = new DrawingWidget(tsp);
+    p_widget->setFixedSize(1600, 900);
 
     QMainWindow main_wnd;
-    main_wnd.setFixedSize(800, 600);
+    main_wnd.setFixedSize(1600, 900);
     main_wnd.layout()->addWidget(p_widget);
 
     main_wnd.show();
 
-    auto path = tsp.GenerateSomePath();
-    auto best_path = tsp.GenerateBestPathStartingFrom(path);
-    p_widget->UpdatePath(best_path);
+    //auto path = tsp.GenerateSomePath();
+    //auto best_path = tsp.GenerateBestPathStartingFrom(path);
+    //p_widget->UpdatePath(best_path);
+
+    auto p_solver = std::make_shared<TSPGenetic::GeneticSolver>(tsp);
+    p_solver->InitializePopulation(100000);
+    
+    const size_t iterations = 100;
+
+    QFutureWatcher<TSP::TPath> watcher;
+    QObject::connect(&watcher, &QFutureWatcher<TSP::TPath>::finished, [&]() {
+        auto result = watcher.result();
+        p_widget->UpdatePath(result);
+        watcher.setFuture(QtConcurrent::run([&]() -> TSP::TPath 
+        {
+            std::this_thread::sleep_for(std::chrono::microseconds(250));
+            return _PerformNextStepAndReturnBestPath(p_solver);
+        }));
+    });
+
+    watcher.setFuture(QtConcurrent::run([&]() -> TSP::TPath
+    {
+        return _PerformNextStepAndReturnBestPath(p_solver);
+    }));
 
     auto return_code = app.exec();
     std::cout << "Terminating application. Exit code: " << return_code << std::endl;
