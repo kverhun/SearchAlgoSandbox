1233f712a1534b9c4d478afc46ce34886c05a8d7
kverhun <k.verhun@gmail.com>
2016-11-04 21:11:16 +0200

    Add qt gui application

--- tasks/kverhun/08-TSP/gui/main.cpp
+++ tasks/kverhun/08-TSP/gui/main.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
 
+#include <QApplication>
+#include <QtWidgets>
+
 int main(int i_argc, char** i_argv)
 {
-    std::cout << "Hello world" << std::endl;
-    return 0;
+    std::cout << "Start application..." << std::endl;
+
+    QApplication app(i_argc, i_argv);
+    
+    QMainWindow main_wnd;
+    main_wnd.show();
+
+    auto return_code = app.exec();
+    std::cout << "Terminating application. Exit code: " << return_code << std::endl;
+    
+    return return_code;
 }
