a9826d69d38ee46973492e4089252e08b3a49047
Kostiantyn Verhun <k.verhun@gmail.com>
2018-05-08 00:06:49 +0300

    put dllexport/import under ifdef

--- src/impl/_Library.h
+++ src/impl/_Library.h
@@ -1,7 +1,11 @@
 #pragma once
 
-#if TSP_LIBRARY
-#define TSP_API __declspec(dllexport)
+#ifdef _WINDOWS
+    #if TSP_LIBRARY
+        #define TSP_API __declspec(dllexport)
+    #else
+        #define TSP_API __declspec(dllimport)
+    #endif
 #else
-#define TSP_API __declspec(dllimport)
+    # define TSP_API
 #endif
